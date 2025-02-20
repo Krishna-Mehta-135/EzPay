f13098d1da366413ffb2c7438bcf881298c9b140
Krishna Mehta <krishnamehta135@protonmail.com>
2025-02-20 20:05:02 +0530

    Added components

---
+++ Client/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '../components/Button'
+import { Heading } from '../components/Heading';
+
+
+const Signin = () => {
+  return (
+    <div>
+      <Heading label={Signin} />
+      <Button label="Krishna"/>
+    </div>
+  )
+}
+
+export default Signin
