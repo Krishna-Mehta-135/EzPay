1e28429f4404cae01085389a04a25d3b4df3dbfa
Krishna Mehta <krishnamehta135@protonmail.com>
2025-03-06 00:54:22 +0530

    Added signup

--- Client/src/components/InputBox.jsx
+++ Client/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-export function InputButton({label, placeholder, onChange}) {
+export function InputBox({label, placeholder, onChange}) {
     return <div>
         <div className="text-sm font-medium text-left py-2">
             {label}
--- Client/src/pages/Signup.jsx
+++ Client/src/pages/Signup.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import {Heading} from "../components/Heading.jsx";
+import {SubHeading} from "./../components/SubHeading.jsx";
+import {InputBox} from "../components/InputBox.jsx";
+import {Button} from "./../components/Button.jsx";
+import { BottomWarning } from './../components/BottomWarning';
+
 
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                    <Heading label={"Sign Up"} />
+                    <SubHeading label={"Enter your infromation to create an account"} />
+                    <InputBox placeholder="John" label={"First Name"} />
+                    <InputBox placeholder="Doe" label={"Last Name"} />
+                    <InputBox placeholder="abcd@gmail.com" label={"Email"} />
+                    <InputBox placeholder="123456" label={"Password"} />
+                </div>
+                <div className="pt-4">
+                    <Button label={"Sign up"} />
+                </div>
+                <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
+            </div>
+        </div>
+    );
+};
 
-export default Signup
+export default Signup;
